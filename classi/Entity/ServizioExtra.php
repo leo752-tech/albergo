c411828b9dd00e8a1ef1ffc0b9eb1cc9ba573b34
leo752-tech <leopoldosilvestri@gmail.com>
2025-05-21 13:15:52 +0200

    Update ServizioExtra.php

--- classi/Entity/ServizioExtra.php
+++ classi/Entity/ServizioExtra.php
@@ -1,4 +1,35 @@
 <?php
-class ServizioExtra
+class ServizioExtra{
+	private $nome;
+	private $descrizione;
+	private $prezzo;
+
+	public function __constructor($nome, $descrizione, $prezzo){
+		$this->nome = $nome;
+		$this->descrizione = $descrizione;
+		$this->prezzo = $prezzo;
+	}
+
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function setDescrizione($descrizione){
+		$this->descrizione = $descrizione;
+	}
+	public function getDescrizione(){
+		return $this->descrizione;
+	}
+
+	public function setPrezzo($prezzo){
+		$this->prezzo = $prezzo;
+	}
+	public function getPrezzo(){
+		return $this->prezzo;
+	}
+}
 
 ?>
