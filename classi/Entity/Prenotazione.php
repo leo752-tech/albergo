66fe8f09e7bc4050b0edc9c8e2556b049d524460
leo752-tech <leopoldosilvestri@gmail.com>
2025-05-22 17:49:48 +0200

    Update Prenotazione.php

--- classi/Entity/Prenotazione.php
+++ classi/Entity/Prenotazione.php
@@ -1,24 +1,38 @@
 <?php
 
-class Prenotazione{
+class EPrenotazione{
 
+	private ?int $idPrenotazione;
 	private $utente;
 	//decidere se elenco di stringhe o elenco di Utenti
 	private $utenti = array();
 	private $periodo;
 	private $camera;
 	private $prezzo;
+	private ?EServizioExtra $servizio;
 	private $cancellazione = false; 
 
-	public function __construct($utente, $utenti, $periodo, $camera){
+	public function __construct($utente, $utenti, $periodo, $camera, ?EServizioExtra $servizio = null){
+		$this->idPrenotazione = null;
 		$this->utente = $utente;
 		$this->utenti = $utenti;
 		$this->periodo = $periodo;
 		$this->camera = $camera;
-		//da modificare in base alle politiche prezzo e al periodo
-		$this->prezzo = ($this->camera->getPrezzo())*($this->periodo->getLunghezza());
+		$this->servizio = $servizio;
+		$this->prezzo = $this->setPrezzo($periodo, $camera, $servizio);
+		//collegamento di questo oggetto con le classi EUtente e ECamera
+		$utente->setPrenotazioni($this);
+		$camera->setPrenotazioni($this);
+		$camera->setOccupazioni($this);
 	}
-//non ci sono i metodi setter perchè la prenotazione quando viene effettuata non può essere modificata, ma solo cancellata
+//non ci sono i metodi setter (fatta eccezione per il prezzo che varia in maniera dinamica) perchè la prenotazione quando viene effettuata non può essere modificata, ma solo cancellata
+	public function setIdPrenotazione($idPrenotazione){
+		$this->idPrenotazione = $idPrenotazione;
+	}
+	public function getIdPrenotazione(){
+		return $this->idPrenotazione;
+	}
+
 	public function getUtente(){
 		return $this->utente;
 	}
@@ -31,12 +45,33 @@ class Prenotazione{
 	public function getCamera(){
 		return $this->camera;
 	}
+	public function getPrezzo(){
+		return $this->prezzo;
+	}
+	private function setPrezzo($periodo, $camera, $servizio){
+		$incremento = 20;
+		$incrementoServizio =  0;
+		//se c'è un servizio l'incremento del prezzo viene aggiornato
+		if(isset($servizio)){$incrementoServizio = $servizio->getPrezzo();}
+		
+		$giorni = $periodo->getLunghezza();
+		$prezzoUnitario = $camera->getPrezzo();
+		if($periodo->getTipo()=="alta"){$prezzoUnitario += $incremento;}
+		if(isset($servizio)){$prezzoUnitario += $incrementoServizio;}
+
+		return $giorni*$prezzoUnitario;
+	}
 	public function getCancellazione(){
 		return $this->cancellazione;
 	}
 	public function setCancellazione(){
 		$this->cancellazione = true;
 	}
+
+	public function getServizio(){
+		return $this->servizio;
+	}
+	
 	 
 
 }
