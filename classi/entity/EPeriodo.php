2319f62d0ed599271de1bc9d5c3aa9cb371090e6
leo752-tech <leopoldosilvestri@gmail.com>
2025-05-22 17:48:43 +0200

    Update EPeriodo.php

--- classi/entity/EPeriodo.php
+++ classi/entity/EPeriodo.php
@@ -1,23 +1,27 @@
 <?php
 
 
-class Periodo{
+class EPeriodo{
 
+	private ?int $idPeriodo;
 	private DateTime $inizio;
 	private DateTime $fine;
 	private $lunghezza;
-	private string $tipo;
+	private $tipo;
 
-	//private $periodi = array("altaStagione"=>$periodiAlta, "bassaStagione"=>$periodiBassa);
-	private $periodiAlta = array();
-	private $periodiBassa = array();
-
-	
 	public function __construct(DateTime $inizio, DateTime $fine){
+		$this->idPeriodo = null;
 		$this->inizio = $inizio;
 		$this->fine = $fine;
 		$this->lunghezza = $this->setLunghezza();
-		
+		$this->tipo = $this->setTipo($inizio, $fine);	
+	}
+
+	public function setIdPeriodo($idPeriodo){
+		$this->idPeriodo = $idPeriodo;
+	}
+	public function getIdPeriodo(){
+		return $this->idPeriodo;
 	}
 
 	public function setInizio($inizio){
@@ -37,45 +41,32 @@ class Periodo{
 		return $this->lunghezza;
 	}
 	private function setLunghezza(){
-		$lunghezza1 = $this->inizio->diff($this->fine);
-		$this->lunghezza = $lunghezza1->days;
-		
+		return $this->inizio->diff($this->fine)->days;	
 	}
 
-	public function setTipo($ini, $fin){
-		$d3 = new DateTime("10-12-2025");
-		$d4 = new DateTime("07-01-2026");
-		$pa1 = new Periodo($d3, $d4);
-
-		$d5 = new DateTime("01-07-2025");
-		$d6 = new DateTime("31-08-2025");
-		$pa2 = new Periodo($d3, $d4);
-		$list = array($pa1, $pa2);
-		$this->setPeriodiAlta($list);
-		
+	private function setTipo($ini, $fin){
+		ConfiguraPeriodi::inizializzaPeriodi(); //permette l'accesso all'array di periodi che si trova nella classe Con
+		$periodiA = ConfiguraPeriodi::$list;
 		$tipo = "bassa";
-		$min = 4; //impostiamo un valore minimo tra l'inizio del periodo di alta stagione e la fine della prenotazione, o la fine del periodo di alta stagione e l'inizio della prenotazione, in modo tale che se la prenotazione occupa pochissimi giorni del periodo di alta, l'utente non sia costretto a pagare l'importo come se stesse in alta stagione
-		foreach ($this->periodiAlta as $periodo) {
-			if(($ini >= $periodo->getInizio() || $fin <= $periodo->getFine()) && ($fin-$periodo->getInizio)>=$min || ($periodo->getFine()-$ini)>=$min){
+
+		for($i=0; $i<count($periodiA); $i++){
+			echo "CICLO " . $i . "<br>";
+			if(($ini > $periodiA[$i][0] && $ini < $periodiA[$i][1]) || ($fin > $periodiA[$i][0] && $fin < $periodiA[$i][1])){
 				$tipo = "alta";
 				break;
 			}
 		}
-		$this->tipo = $tipo;
-    }
+		return $tipo;
+	}
 
 	public function getTipo(){
 		return $this->tipo;
 	}
 
-	public function setPeriodiAlta($periodi){
-		$this->periodiAlta = $periodi;
-
+	public function getStringPeriodo(){
+		$output = $this->inizio->format("d-m-Y") . " - " . $this->fine->format("d-m-Y");
+		return $output;
 	}
-
-
-
-
 }
 
 ?>
