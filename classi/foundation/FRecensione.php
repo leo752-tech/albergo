8dbfe974741e18a0a80721d87b761382d4e730ce
leo752-tech <leopoldosilvestri@gmail.com>
2025-05-24 20:08:14 +0200

    Update FRecensione.php

--- classi/foundation/FRecensione.php
+++ classi/foundation/FRecensione.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 class FRecensione{
 
@@ -8,15 +8,16 @@ class FRecensione{
    
     private static $table = "recensione";
    
-    private static $values = "(NULL,:titolo,:valutazione,:descrizione,:data)";
+    private static $values = "(NULL,:titolo,:valutazione,:descrizione,:data,:idUtente)";
     public function __construct(){}
 
     //metodo che 
-    public static function bind($stmt,$recensione) {
+    public static function bind($stmt,$recensione) { 
         $stmt->bindValue(":titolo", $recensione->getTitolo(), PDO::PARAM_STR);
-        $stmt->bindValue("valutazione", $recensione->getValutazione(), PDO::PARAM_INT);
-        $stmt->bindValue("descrizione", $recensione->getDescrizione(), PDO::PARAM_STR);
-        $stmt->bindValue("data", $recensione->getData()->format('Y-m-d H:i:s'), PDO::PARAM_STR);    
+        $stmt->bindValue(":valutazione", $recensione->getValutazione(), PDO::PARAM_INT);
+        $stmt->bindValue(":descrizione", $recensione->getDescrizione(), PDO::PARAM_STR);
+        $stmt->bindValue(":data", $recensione->getData(), PDO::PARAM_STR);
+        $stmt->bindValue(":idUtente", $recensione->getIdUtente(), PDO::PARAM_INT);     
     }
 
     public static function getKey(){
@@ -35,16 +36,16 @@ class FRecensione{
         return self::$values;
     }
 
-    //crea un oggetto ERecensione
+    //crea un oggetto ECamera
     public static function creaRecensione($queryRes){
-        $recensione = new ERecensione($queryRes["titolo"], $queryRes["valutazione"], $queryRes["descrizione"], $queryRes["data"]);
+        $recensione = new ERecensione($queryRes["titolo"], $queryRes["valutazione"], $queryRes["descrizione"], $queryRes["data"], $queryRes["idUtente"]);
         if (isset($queryRes["idRecensione"])) { 
             $recensione->setIdRecensione($queryRes["idRecensione"]);
         }
         return $recensione;
     }
 
-    //recupera un oggetto ERecensione tramite il suo id
+    //recupera un oggetto EUtente tramite il suo id
     public static function getRecensione($id){
         $result = FDataMapper::getInstance()->recuperaOggetto(self::$table, self::$key, $id);
         if($result != false && $result != null){
@@ -55,20 +56,23 @@ class FRecensione{
         }
     }
 
-//salva l'oggetto recensione se non esiste, altrimenti fa un aggiornamento
+    //salva l'oggetto utente se non esiste, altrimenti fa un aggiornamento
     public static function salvaRecensione($oggetto , $campi = null){
         if($campi === null){
-            $recensione = FDataMapper::getInstance()->salvaOggetto(self::$class, $oggetto);
-            if($recensione !== null){
-                return $recensione;
+            $id = FDataMapper::getInstance()->salvaOggetto(self::$class, $oggetto);
+            if($id !== null){
+                $oggetto->setIdRecensione($id);
+                return $id;
             }else{
                 return false;
             }
-        }else{
+            
+            }
+        else{
             try{
                 FDataMapper::getInstance()->getDb()->beginTransaction();
                 foreach($campi as $c){
-                    FDataMapper::getInstance()->aggiornaOggetto(self::$table, $c[0], $c[1], self::$key, $oggetto->getIdCamera());
+                    FDataMapper::getInstance()->aggiornaOggetto(self::$table, $c[0], $c[1], self::$key, $oggetto->getIdRecensione());
                 }
                 FDataMapper::getInstance()->getDb()->commit();
                 return true;
@@ -79,7 +83,7 @@ class FRecensione{
             }
         }
     }
-    
+
     //cancella un oggetto dal db
     public static function cancellaRecensione($id){
         try{
