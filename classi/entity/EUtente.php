e615f73cf4ccca4ac919cc0f981480aca98d058c
leo752-tech <leopoldosilvestri@gmail.com>
2025-05-24 20:03:30 +0200

    Update EUtente.php

--- classi/entity/EUtente.php
+++ classi/entity/EUtente.php
@@ -1,21 +1,15 @@
 <?php
 
-class EUtente{
+class Eutente{
 	
-	private ?int $idUtente;
-	private string $mail;
-	private string $password;
-	private string $nome;
-	private string $cognome;
-	private string $dataN;
-	private string $comuneN;
-	private $prenotazioni = array();
-	private $recensioni = array();
+	protected ?int $idUtente;
+	protected string $nome;
+	protected string $cognome;
+	protected string $dataN;
+	protected string $comuneN;
 
-	public function __construct($mail, $password, $nome, $cognome, $dataN, $comuneN){
+	public function __construct($nome, $cognome, $dataN, $comuneN){
 		$this->idUtente = null;
-		$this->mail = $mail;
-		$this->password = $password;
 		$this->nome = $nome;
 		$this->cognome = $cognome;
 		$this->dataN = $dataN;
@@ -27,20 +21,6 @@ class EUtente{
 	}
 	public function getIdUtente(){
 		return $this->idUtente;
-	}
-
-	public function setMail(string $Mail){
-		$this->mail = $mail;
-	}
-	public function getMail(){
-		return $this->mail;
-	}
-
-	public function setPassword(string $password){
-		$this->password = $password;
-	}
-	public function getPassword(){
-		return $this->password;
 	}
 
 	public function setNome(string $nome){
@@ -66,19 +46,6 @@ class EUtente{
 	}
 	public function getComuneN(){
 		return $this->comuneN;
-	}
-	public function setPrenotazioni($prenotazione){
-		$this->prenotazioni[] = $prenotazione;
-	}
-	public function getPrenotazioni(){
-		return $this->prenotazioni;
-	}
-
-	public function setRecensioni($recensione){
-		$this->recensioni[] = $recensione;
-	}
-	public function getRecensioni(){
-		return $this->recensioni;	
 	}
 }
 
