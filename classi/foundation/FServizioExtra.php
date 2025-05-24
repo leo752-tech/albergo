1f5476b3a0e45c5478c2b4f6a4489212464489d8
leo752-tech <leopoldosilvestri@gmail.com>
2025-05-24 20:08:37 +0200

    Update FServizioExtra.php

--- classi/foundation/FServizioExtra.php
+++ classi/foundation/FServizioExtra.php
@@ -6,19 +6,17 @@ class FServizioExtra{
 
     private static $class = "FServizioExtra";
    
-    private static $table = "servizioExtra";
+    private static $table = "servizioextra";
    
-    private static $values = "(:idServizioExtra,:nome,:descrizione,:prezzo,:idPrenotazione)";
+    private static $values = "(NULL,:nome,:descrizione,:prezzo,:idPrenotazione)";
     public function __construct(){}
 
     //metodo che 
     public static function bind($stmt,$servizioExtra) {
-        
-        $stmt->bindValue("idServizioExtra", $servizioExtra->getIdServizioExtra(), PDO::PARAM_INT);
         $stmt->bindValue("nome", $servizioExtra->getNome(), PDO::PARAM_STR);
         $stmt->bindValue("descrizione", $servizioExtra->getDescrizione(), PDO::PARAM_STR);    
         $stmt->bindValue("prezzo", $servizioExtra->getPrezzo(), PDO::PARAM_STR);
-        $stmt->bindValue("idPrenotazione", $servizioExtra->getPrenotazione()->getId(), PDO::PARAM_INT);
+        $stmt->bindValue("idPrenotazione", $servizioExtra->getIdPrenotazione(), PDO::PARAM_INT);
     }
 
     public static function getKey(){
@@ -37,26 +35,30 @@ class FServizioExtra{
         return self::$values;
     }
 
-    public static function creaOggetto($queryRes){
-        $servizioExtra = new EServizioExtra($queryRes["idServizioExtra"], $queryRes["nome"], $queryRes["descrizione"], $queryRes["prezzo"], $queryRes["idPrenotazione"]);
+    public static function creaServizioExtra($queryRes){
+        $servizioExtra = new EServizioExtra($queryRes["nome"], $queryRes["descrizione"], $queryRes["prezzo"], $queryRes["idPrenotazione"]);
+        if (isset($queryRes["idServizioExtra"])) { 
+            $servizioExtra->setIdServizioExtra($queryRes["idServizioExtra"]);
+        }
         return $servizioExtra;
     }
 
-    public static function getOggetto($id){
+    public static function getServizioExtra($id){
         $result = FDataMapper::getInstance()->recuperaOggetto(self::$table, self::$key, $id);
-        if(count($result) > 0){
-            $servizioExtra = self::creaOggetto($result);
+        if($result != false && $result != null){
+            $servizioExtra = self::creaServizioExtra($result);
             return $servizioExtra;
         }else{
             return null;
         }
     }
 
-    public static function salvaOggetto($oggetto , $campi = null){
+    public static function salvaServizioExtra($oggetto , $campi = null){
         if($campi === null){
-            $servizioExtra = FDataMapper::getInstance()->salvaOggetto(self::$class, $oggetto);
-            if($servizioExtra !== null){
-                return $servizioExtra;
+            $id = FDataMapper::getInstance()->salvaOggetto(self::$class, $oggetto);
+            if($id !== null){
+                $oggetto->setIdServizioExtra($id);
+                return $id;
             }else{
                 return false;
             }
@@ -64,7 +66,7 @@ class FServizioExtra{
             try{
                 FDataMapper::getInstance()->getDb()->beginTransaction();
                 foreach($campi as $c){
-                    FDataMapper::getInstance()->aggiornaOggetto(self::$table, $c[0], $c[1], self::$key, $oggetto->getId());
+                    FDataMapper::getInstance()->aggiornaOggetto(self::$table, $c[0], $c[1], self::$key, $oggetto->getIdServizioExtra());
                 }
                 FDataMapper::getInstance()->getDb()->commit();
                 return true;
