f000abd503527713e68d0c02a6b64bcdf9888208
leo752-tech <leopoldosilvestri@gmail.com>
2025-05-27 17:36:31 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,16 +4,30 @@ USE my_database_prova;
 
 /*
  * Tabella: utenti
- * Rappresenta gli utenti del sistema (clienti che prenotano, ospiti aggiuntivi)
+ * Rappresenta gli utenti del sistema che sono registrati e effettuano prenotazioni
  */
-CREATE TABLE IF NOT EXISTS utenti (
-    id_utente INT PRIMARY KEY AUTO_INCREMENT,
-    mail VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL, # Conserva l'hash della password. Assicurati che la tua Entity salvi un hash.
+CREATE TABLE IF NOT EXISTS utenti_registrati (
+    idUtente INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     cognome VARCHAR(100) NOT NULL,
-    data_nascita DATE NOT NULL,
-    comune_nascita VARCHAR(100) NOT NULL
+    dataN DATE NOT NULL,
+    comuneN VARCHAR(100) NOT NULL
+    mail VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, # Conserva l'hash della password. Assicurati che la tua Entity salvi un hash.
+    idPrenotazioni VARCHAR(255) # formato json che conserva uun array dove ogni elemento corrisponde all'id di una prenotazione effettuata
+    idRecensioni VARCHAR(255) # formato json che conserva uun array dove ogni elemento corrisponde all'id di una recensione effettuata   
+);
+
+/*
+ * Tabella: utenti
+ * Rappresenta gli utenti del sistema che soggiornano nella struttura
+ */
+CREATE TABLE IF NOT EXISTS utenti (
+    idUtente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cognome VARCHAR(100) NOT NULL,
+    dataN DATE NOT NULL,
+    comuneN VARCHAR(100) NOT NULL
 );
 
 /*
@@ -21,11 +35,13 @@ CREATE TABLE IF NOT EXISTS utenti (
  * Rappresenta le camere disponibili nell'hotel
  */
 CREATE TABLE IF NOT EXISTS camere (
-    id_camera INT PRIMARY KEY AUTO_INCREMENT,
+    idCamera INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     posti INT NOT NULL,
-    prezzo_notte DECIMAL(10, 2) NOT NULL,
+    prezzo DECIMAL(10, 2) NOT NULL,
     tipo VARCHAR(50) NOT NULL # Es: 'Singola', 'Doppia', 'Suite'
+    idOccupazioni TEXT, # formato json che memorizza gli id di tutti i periodi durante i quali la camera è occupata
+    idPrenotazioni TEXT # formato json che memorizza gli id di tutte le prenotazioni effettuate su quella camera 
 );
 
 /*
@@ -37,6 +53,8 @@ CREATE TABLE IF NOT EXISTS servizi_extra (
     nome VARCHAR(100) NOT NULL UNIQUE,
     descrizione TEXT,
     prezzo DECIMAL(10, 2) NOT NULL
+    idPrenotazione INT #------------------DA VEDERE-----------------
+
 );
 
 /*
@@ -44,19 +62,19 @@ CREATE TABLE IF NOT EXISTS servizi_extra (
  * Rappresenta le prenotazioni effettuate dai clienti
  */
 CREATE TABLE IF NOT EXISTS prenotazioni (
-    id_prenotazione INT PRIMARY KEY AUTO_INCREMENT,
-    id_utente_prenotante INT NOT NULL, # Utente che ha effettuato la prenotazione
-    id_camera INT NOT NULL,
-    data_check_in DATE NOT NULL,
-    data_check_out DATE NOT NULL,
-    prezzo_totale DECIMAL(10, 2) NOT NULL,
-    id_servizio_extra INT NULL, # Può essere NULL se nessun servizio extra è stato scelto
-    data_prenotazione DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    idPrenotazione INT PRIMARY KEY AUTO_INCREMENT,
+    idUtente INT NOT NULL, # Utente che ha effettuato la prenotazione
+    idUtenti VARCHAR(255), # Utenti ospiti che soggiornano nella camera
+    idCamera INT NOT NULL,
+    idPeriodo INT NOT NULL,
+    idServizioExtra INT NULL, # Può essere NULL se nessun servizio extra è stato scelto
+    prezzo DECIMAL(10, 2) NOT NULL, #------------------DA VEDERE----------------------
+    dataPrenotazione DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     cancellata BOOLEAN DEFAULT FALSE,
     
-    FOREIGN KEY (id_utente_prenotante) REFERENCES utenti(id_utente) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (id_camera) REFERENCES camere(id_camera) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (id_servizio_extra) REFERENCES servizi_extra(id_servizio) ON DELETE SET NULL ON UPDATE CASCADE
+    FOREIGN KEY (idUtente) REFERENCES utenti_registrati(idUtente) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (idCamera) REFERENCES camere(idCamera) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (idServizioExtra) REFERENCES servizi_extra(idServizioExtra) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 /*
@@ -64,12 +82,12 @@ CREATE TABLE IF NOT EXISTS prenotazioni (
  * Gestisce la relazione tra prenotazioni e utenti (gli ospiti aggiuntivi)
  */
 CREATE TABLE IF NOT EXISTS prenotazioni_utenti (
-    id_prenotazione INT NOT NULL,
-    id_utente INT NOT NULL, # ID dell'ospite (può essere lo stesso del prenotante, ma qui rappresenta un ospite sulla prenotazione)
-    PRIMARY KEY (id_prenotazione, id_utente), # Chiave primaria composta
+    idPrenotazione INT NOT NULL,
+    idUtente INT NOT NULL, # ID dell'utente che ha effettuato la prenotazione  -----------DA VEDERE------------------
+    PRIMARY KEY (idPrenotazione, idUtente), # Chiave primaria composta
     
-    FOREIGN KEY (id_prenotazione) REFERENCES prenotazioni(id_prenotazione) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (id_utente) REFERENCES utenti(id_utente) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (idPrenotazione) REFERENCES prenotazioni(idPrenotazione) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idUtente) REFERENCES utenti_registrati(idUtente) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 /*
@@ -77,12 +95,25 @@ CREATE TABLE IF NOT EXISTS prenotazioni_utenti (
  * Rappresenta le recensioni scritte dagli utenti
  */
 CREATE TABLE IF NOT EXISTS recensioni (
-    id_recensione INT PRIMARY KEY AUTO_INCREMENT,
-    id_utente INT NOT NULL, # Utente che ha scritto la recensione
+    idRecensione INT PRIMARY KEY AUTO_INCREMENT,
+    idUtente INT NOT NULL, # Utente che ha scritto la recensione
     titolo VARCHAR(255),
     valutazione INT NOT NULL, # Es: da 1 a 5
     descrizione TEXT,
-    data_recensione DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    dataRecensione DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     
-    FOREIGN KEY (id_utente) REFERENCES utenti(id_utente) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (idUtente) REFERENCES utenti_registrati(idUtente) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+/*
+ * Tabella: periodi
+ * Rappresenta i periodi di tutte le prenotazioni
+*/
+CREATE TABLE IF NOT EXISTS periodi (
+    idPeriodo INT PRIMARY KEY AUTO_INCREMENT,
+    inizio DATE NOT NULL,
+    fine DATE NOT NULL,
+    lunghezza INT NOT NULL,
+    tipo VARCHAR(255),
+    idPrenotazione #---------------DA VEDERE-------------------------------- 
+)
