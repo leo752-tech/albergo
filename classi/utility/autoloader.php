b0f8d9f7e9011de0664181abaef96a98ed75e524
leo752-tech <leopoldosilvestri@gmail.com>
2025-06-03 15:55:18 +0200

    Add files via upload

---
+++ classi/utility/autoloader.php
@@ -0,0 +1,19 @@
+<?php
+ 
+function my_autoloader($className) {
+    $firstLetter = $className[0];
+    switch ($firstLetter) {
+        case 'E':
+            include_once(__DIR__ . '/../entity/' . $className . '.php' );
+            break;
+        case 'F':
+            include_once(__DIR__ . "/../foundation/" . $className . '.php' );
+            break;
+        case 'U':
+            include_once(__DIR__ . "/../utility/" . $className . '.php' );   
+        // ... altri casi ...
+        
+    }
+}
+
+spl_autoload_register('my_autoloader');
