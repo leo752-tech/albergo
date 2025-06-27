eb256afdf532a0ac30da77826c0df287bc74df30
Federico Palmerini <fedepalme25@gmail.com>
2025-06-27 16:21:48 +0200

    a

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,11 @@
 <?php
 
 //Database Connection
-/*
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'hotel_db');
 define('DB_USER', 'root');
-define('DB_PASS', '');*/
+define('DB_PASS', '');
 
 
 //session coockie expiration
---
+++ install/installation.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once(__DIR__ . '/../../config/config.php');
+/**
+ * class for checking if the db exist and if not create it
+ */
+class Installation{
+    public static function install(){
+        try{
+            $conn =  new PDO("mysql:host=".DB_HOST."; charset=utf8;", DB_USER, DB_PASS);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $conn->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '" . DB_NAME . "'");
+            if($stmt->rowCount() == 0){
+                // Database does not exist, create it
+                
+                $sql = "CREATE DATABASE " . DB_NAME;
+                $conn->exec($sql);
+                $conn->exec("USE " . DB_NAME);
+                $sqlFile = __DIR__ . '/database.sql';
+                if (!file_exists($sqlFile)) {
+                    throw new Exception("SQL file not found: " . $sqlFile);
+                }
+                $sql = file_get_contents($sqlFile);
+                $conn->exec($sql);
+                $popolaSqlFile = __DIR__ . '/popolaDb.sql';
+                if (!file_exists($popolaSqlFile)) {
+                    throw new Exception("SQL file not found: " . $popolaSqlFile);
+                }
+                $dataSqlCommands = file_get_contents($popolaSqlFile);
+                $conn->exec($dataSqlCommands); 
+
+                error_log("Database '" . DB_NAME . "' successfully created and populated.");
+            } else {
+                error_log("Database '" . DB_NAME . "' already exists. No installation performed.");
+            }
+            $conn = null;
+            return true;
+        }catch(PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+            return false;
+        }
+    }
+}
