7f88aeb2212516202ff20cf8e5baffa40d8afcd1
leo <leopoldosilvestri@gmail.com>
2025-06-28 16:03:17 +0200

    basta

--- app/prova.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'C:\Users\Federico\OneDrive\Desktop\new\albergo\app\Utility\autoloader.php';
-$u1= new ERegisteredUser(null,11,'fedepalme@live.it','federico','Federico','Palmerini',new DateTime('2002-01-25'),'Pescara');
-//$r = FPersistentManager::getInstance()->saveObject($u1);
-//$m=[['price', 49.99]];
-//$r = FPersistentManager::getInstance()->updateObject(FPersistentManager::getInstance()->getObject('EExtraService', 5),$m);
-$r = FPersistentManager::getInstance()->saveObject($u1);
